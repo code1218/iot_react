9c04057a702cbc40801c435e7b8270353b3e3210
code1218 <skjil1218@gmail.com>
2024-12-17 09:21:08 +0900

    react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Promise1 from './study/Promise1';
+import AsyncAwait, {test1, test2} from './study/AsyncAwait';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
